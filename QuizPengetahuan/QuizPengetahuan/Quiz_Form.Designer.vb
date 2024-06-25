bbe1be033f71aa3bc386bf8ad3450b886318d1d0
Litto <118559955+kaittozu@users.noreply.github.com>
2024-06-25 08:40:52 +0700

    Add files via upload

---
+++ QuizPengetahuan/QuizPengetahuan/Quiz_Form.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class QuizPengetahuan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonNext = New System.Windows.Forms.Button()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.richTextBox_Question = New System.Windows.Forms.RichTextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ButtonNext
+        '
+        Me.ButtonNext.BackColor = System.Drawing.Color.Green
+        Me.ButtonNext.FlatAppearance.BorderSize = 0
+        Me.ButtonNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ButtonNext.Font = New System.Drawing.Font("Gotham", 21.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonNext.ForeColor = System.Drawing.Color.White
+        Me.ButtonNext.Location = New System.Drawing.Point(12, 467)
+        Me.ButtonNext.Name = "ButtonNext"
+        Me.ButtonNext.Size = New System.Drawing.Size(629, 61)
+        Me.ButtonNext.TabIndex = 11
+        Me.ButtonNext.Text = "Selanjutnya"
+        Me.ButtonNext.UseVisualStyleBackColor = False
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Font = New System.Drawing.Font("Gotham", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton4.Location = New System.Drawing.Point(414, 357)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(224, 35)
+        Me.RadioButton4.TabIndex = 10
+        Me.RadioButton4.Text = "RadioButton4"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Font = New System.Drawing.Font("Gotham", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton3.Location = New System.Drawing.Point(414, 285)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(222, 35)
+        Me.RadioButton3.TabIndex = 9
+        Me.RadioButton3.Text = "RadioButton3"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Gotham", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(15, 357)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(222, 35)
+        Me.RadioButton2.TabIndex = 8
+        Me.RadioButton2.Text = "RadioButton2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Gotham", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(15, 283)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(216, 35)
+        Me.RadioButton1.TabIndex = 7
+        Me.RadioButton1.Text = "RadioButton1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'richTextBox_Question
+        '
+        Me.richTextBox_Question.Font = New System.Drawing.Font("Gotham", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.richTextBox_Question.Location = New System.Drawing.Point(3, 3)
+        Me.richTextBox_Question.Name = "richTextBox_Question"
+        Me.richTextBox_Question.Size = New System.Drawing.Size(623, 123)
+        Me.richTextBox_Question.TabIndex = 6
+        Me.richTextBox_Question.Text = ""
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Gray
+        Me.Panel1.Controls.Add(Me.richTextBox_Question)
+        Me.Panel1.Location = New System.Drawing.Point(12, 102)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(629, 129)
+        Me.Panel1.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Gotham", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(194, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(310, 31)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "QUIZ PENGETAHUAN"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Gotham", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(182, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(338, 40)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "S E D E R H A N A"
+        '
+        'QuizPengetahuan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(657, 540)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.ButtonNext)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Name = "QuizPengetahuan"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Quiz Pengetahuan"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ButtonNext As System.Windows.Forms.Button
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents richTextBox_Question As System.Windows.Forms.RichTextBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
---
+++ QuizPengetahuan/QuizPengetahuan/Quiz_Form.vb
@@ -0,0 +1,153 @@
+﻿Public Class QuizPengetahuan
+
+    ' array of questions
+    Dim questions() As String = {"Presiden pertama indonesia adalah ..", "100 + 1000 = ?", "5000 + 10.000 = ?", "Langit berwana ..", "100 - 100 = ?"}
+    ' array of options
+    Dim options(,) As String = {{"Jokowi", "Prabowo", "Soekarno", "Megawati", "Soekarno"},
+                                               {"1100", "500", "900", "2000", "1100"},
+                                                {"1700", "15000", "20000", "30000", "15000"},
+                                                 {"Biru", "Merah", "Putih", "Hitam", "Biru"},
+                                                  {"0", "1", "90", "100", "0"}}
+
+    Dim index As Integer = 0
+    Dim correct As Integer = 0
+
+    ' create a sub to check the iption selected bu the user
+    ' if it's correct or not
+    Sub checkAnswer(rbt As RadioButton)
+
+        If rbt.Text.Equals(options(index, 4)) Then
+            correct += 1
+            rbt.BackColor = Color.Green
+
+        Else
+            rbt.BackColor = Color.Red
+        End If
+
+        index += 1
+        setEnable(False)
+
+    End Sub
+
+    ' uncheck the radiobuttons
+    Sub uncheck()
+
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        RadioButton4.Checked = False
+
+    End Sub
+
+    ' reset radiobuttons color
+    Sub resetRadio()
+
+        For Each c As Control In Me.Controls
+
+            If TypeOf c Is RadioButton Then
+
+                Dim rdb As RadioButton = c
+                rdb.BackColor = Color.White
+
+            End If
+
+        Next
+
+    End Sub
+
+    ' enable or disable radiobuttons
+    Sub setEnable(yes_or_no As Boolean)
+
+        RadioButton1.Enabled = yes_or_no
+        RadioButton2.Enabled = yes_or_no
+        RadioButton3.Enabled = yes_or_no
+        RadioButton4.Enabled = yes_or_no
+
+    End Sub
+
+    Private Sub Quiz_Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ButtonNext.PerformClick()
+
+    End Sub
+
+    Private Sub ButtonNext_Click(sender As Object, e As EventArgs) Handles ButtonNext.Click
+
+
+
+        resetRadio()
+
+        ' restart the quiz
+        If ButtonNext.Text.Equals("Mulai ulang Quiz") Then
+            index = 0
+            correct = 0
+            richTextBox_Question.BackColor = Color.White
+            richTextBox_Question.ForeColor = Color.Black
+            ButtonNext.Text = "Selanjutnya"
+        End If
+
+        ' display the score
+        If index = questions.Length Then
+            richTextBox_Question.Text = "Skormu adalah: " & correct & " / " & questions.Length
+
+            If correct >= CSng(questions.Length) / 2 Then
+                richTextBox_Question.BackColor = Color.Green
+                richTextBox_Question.ForeColor = Color.White
+            Else
+                richTextBox_Question.BackColor = Color.Red
+                richTextBox_Question.ForeColor = Color.White
+            End If
+
+            ButtonNext.Text = "Mulai ulang Quiz"
+        Else
+            setEnable(True)
+            uncheck()
+            richTextBox_Question.Text = questions(index)
+            RadioButton1.Text = options(index, 0)
+            RadioButton2.Text = options(index, 1)
+            RadioButton3.Text = options(index, 2)
+            RadioButton4.Text = options(index, 3)
+
+            ' if it's the last question
+            If index = questions.Length - 1 Then
+                ButtonNext.Text = "Selesai dan Lihat Skormu"
+            End If
+        End If
+
+    End Sub
+
+    Private Sub RadioButton1_Click(sender As Object, e As EventArgs) Handles RadioButton1.Click
+
+        checkAnswer(RadioButton1)
+
+    End Sub
+
+    Private Sub RadioButton2_Click(sender As Object, e As EventArgs) Handles RadioButton2.Click
+
+        checkAnswer(RadioButton2)
+
+    End Sub
+
+    Private Sub RadioButton3_Click(sender As Object, e As EventArgs) Handles RadioButton3.Click
+
+        checkAnswer(RadioButton3)
+
+    End Sub
+
+    Private Sub RadioButton4_Click(sender As Object, e As EventArgs) Handles RadioButton4.Click
+
+        checkAnswer(RadioButton4)
+
+    End Sub
+
+
+    Private Sub soal_Click(sender As Object, e As EventArgs)
+
+
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+
+    End Sub
+End Class
